094c4bce0249c073c8609b364d01054caef3fade
krish passey <143809157+krishpassey@users.noreply.github.com>
2025-06-20 08:59:44 +0000

    variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,12 @@
+const accountId = 12333
+let accountEmail = "krispassey@gmail.com"
+var acccountPassword = "12345"
+accountCity = "Jaipur"
+
+// accountId = 2 
+
+accountEmail = "krish@gmail.com"
+acccountPasswordn= "21212121"
+accountCity = "bengalore"
+
+console.table ([accountId,accountEmail,acccountPassword,accountCity]);
