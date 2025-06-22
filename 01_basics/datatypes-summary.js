7c5f94a312c80b7c8c6ba8941dd4f492e7d7f903
krish passey <143809157+krishpassey@users.noreply.github.com>
2025-06-22 21:47:39 +0000

    datatype summary

---
+++ 01_basics/datatypes-summary.js
@@ -0,0 +1,42 @@
+// # Primitive
+
+// 7 types : String, Number, Boolean, Null, Undefined, Symbol, BigInt
+
+const score = 100 
+const scoreValue = 100.3
+
+
+const isLoggedIn = false
+const outsideTemp = null
+let userEmail; 
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+console.log(id === anotherId);
+
+
+const bigNumber = 3377454443n 
+
+
+
+// Reference (Non Primitive)
+
+// Array, Objects, Functions 
+
+
+const heros = ["ironman", "superman", "spiderman"];
+
+let myObj = {
+    name: "krish",
+    age: 21,
+}
+
+const myFunction = function(){
+    console.log('hell world');
+}
+
+
+console.log(typeof bigNumber);
+
+
