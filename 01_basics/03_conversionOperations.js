30bc2503862b4e16ae4ce927ad28ec11c02e0b59
krish passey <143809157+krishpassey@users.noreply.github.com>
2025-06-22 10:21:21 +0000

    operations

--- 01_basics/03_conversionOperations.js
+++ 01_basics/03_conversionOperations.js
@@ -27,5 +27,38 @@ console.log(booleanIsLoggedIn);
 let someNumber = 33
 
 let stringNumber = string ( someNumber)
-console.log(stringNumber);
-console.log(typeof stringNumber);
+//console.log(stringNumber);
+//console.log(typeof stringNumber);
+
+
+// *********** operations ************
+let value = 3
+let negValue = -value 
+// console.log(negValue);
+
+
+let str1= "hello"
+let str2 = " krish"
+
+let str3 = str1+str2 
+//console.log(str3);
+
+//console.log("1" + 2);
+//console.log(1 + "2");
+//console.log("1" + 2 + 2);
+//console.log(1 + 2 + "2");
+
+//console.log( (3 + 4) * 5 % 3);
+
+//console.log(+true);
+//console.log(+"")
+
+
+let num1, num2 , num3 
+num1 = num2 = num3 = 2 + 2
+
+let gameCounter = 100
+gameCounter++;
+console.log(gameCounter);
+
+
